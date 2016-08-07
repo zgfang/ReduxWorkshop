27271d94bb88971e205a998caa5ac538560e3be0
Zhigang Fang <zhigang1992@gmail.com>
2016-08-08 02:31:43 +0800

    bring back the todo example

--- js/App.js
+++ js/App.js
@@ -1,46 +1,90 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react'
 import {
+  AppRegistry,
   StyleSheet,
   Text,
   View
-} from 'react-native';
+} from 'react-native'
 
-class App extends Component {
+type Todo = {
+  id: number,
+  name: string,
+  completed: boolean,
+}
+
+const defaultTodos: Array<Todo> = []
+
+const addTodoAction = {
+  type: 'ADD_TODO',
+  name: '记得买牛奶'
+}
+
+function addTodoReducer(todos: Array<Todo>, action): Array<Todo> {
+  if (action.type === 'ADD_TODO') {
+    return [...todos, {
+      id: todos.length,
+      name: action.name,
+      completed: false
+    }]
+  }
+  return todos;
+}
+
+const toggleTodoAction = {
+  type: 'TOGGLE_TODO',
+  id: 0,
+}
+
+function toggleTodoReducer(todos, action) {
+  if (action.type === 'TOGGLE_TODO') {
+    return todos.map(todo=>{
+      if (todo.id === action.id) {
+        return {
+          ...todo,
+          completed: !todo.completed
+        }
+      }
+      return todo
+    })
+  }
+  return todos
+}
+
+function merge(reducers) {
+  return (state, action) => {
+    return reducers.reduce( (loopState, reducer) => {
+      return reducer(loopState, action)
+    }, state)
+  }
+}
+
+const totalReducer = merge([addTodoReducer, toggleTodoReducer])
+
+
+let result = totalReducer(defaultTodos, addTodoAction)
+result = totalReducer(result, addTodoAction)
+result = totalReducer(result, toggleTodoAction)
+
+
+
+class CPICLife extends Component {
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Text>{JSON.stringify(result)}</Text>
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#F5FCFF',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+})
 
-export default App
+export default CPICLife
